f445342b0188a60fee411ab52a478eef3db36c36
Himanshu Patel <himprapatel@gmail.com>
2025-12-09 17:35:57 +0000

    fix(api): Define api_router before including products router

--- backend/app/api/v1/__init__.py
+++ backend/app/api/v1/__init__.py
@@ -1,12 +1,6 @@
 from fastapi import APIRouter
 from app.api.v1.endpoints import auth, internal, products, scan
 
-api_router.include_router(
-    products.router,
-    prefix="/products",
-    tags=["ML Products"]
-)
-
 api_router = APIRouter()
 
 api_router.include_router(
@@ -25,4 +19,10 @@ api_router.include_router(
     scan.router,
     prefix="/scan",
     tags=["Face Scan"]
+)
+
+api_router.include_router(
+    products.router,
+    prefix="/products",
+    tags=["ML Products"]
 )

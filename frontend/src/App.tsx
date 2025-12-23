7759bc3c7bb4599e5b915ed19fa22fd245c40211
Himanshu Patel <himprapatel@gmail.com>
2025-12-23 10:37:22 +0000

    feat(sprint-5): Add routes for AnalysisResults and Recommendations pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ScanPage from "./pages/ScanPage";
+import AnalysisResults from "./pages/AnalysisResults";
+import Recommendations from "./pages/Recommendations";
 
 export default function App() {
   return (
@@ -9,6 +11,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/scan" element={<ScanPage />} />
+                    <Route path="/analysis/:analysisId" element={<AnalysisResults />} />
+            <Route path="/recommendations" element={<Recommendations />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>

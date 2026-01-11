4f038335b5ddfeca429f2d0b7bddff4627f3059e
Himanshu Patel <himprapatel@gmail.com>
2026-01-11 18:43:23 +0000

    Wrap routes with AuthProvider for authentication

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,7 @@
 // src/App.tsx
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import { AuthPage } from "./pages/AuthPage";
 import HomePage from "./pages/HomePage";
 import ScanPage from "./pages/ScanPage";
 import AnalysisResults from "./pages/AnalysisResults";
@@ -7,14 +9,17 @@ import Recommendations from "./pages/Recommendations";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/scan" element={<ScanPage />} />
-                    <Route path="/analysis/:analysisId" element={<AnalysisResults />} />
-            <Route path="/recommendations" element={<Recommendations />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/auth" element={<AuthPage />} />
+          <Route path="/scan" element={<ScanPage />} />
+          <Route path="/analysis/:analysisId" element={<AnalysisResults />} />
+          <Route path="/recommendations" element={<Recommendations />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }

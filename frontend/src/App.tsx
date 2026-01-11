e3ddafbfe42deddb933d2a31908547f1ba64bba8
Himanshu Patel <himprapatel@gmail.com>
2026-01-11 19:15:13 +0000

    Add new routes for onboarding and profile pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,14 @@ import HomePage from "./pages/HomePage";
 import ScanPage from "./pages/ScanPage";
 import AnalysisResults from "./pages/AnalysisResults";
 import Recommendations from "./pages/Recommendations";
+import OnboardingPage from "./pages/OnboardingPage";
+import ProfileSettingsPage from "./pages/ProfileSettingsPage";
+import ConsentPage from "./pages/ConsentPage";
+import MyShelfPage from "./pages/MyShelfPage";
+import ProductDetailsPage from "./pages/ProductDetailsPage";
+import RoutineBuilderPage from "./pages/RoutineBuilderPage";
+import HistoryPage from "./pages/HistoryPage";
+import DashboardPage from "./pages/DashboardPage";
 
 export default function App() {
   return (
@@ -17,7 +25,15 @@ export default function App() {
           <Route path="/scan" element={<ScanPage />} />
           <Route path="/analysis/:analysisId" element={<AnalysisResults />} />
           <Route path="/recommendations" element={<Recommendations />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
+          <Route path="/onboarding" element={<OnboardingPage />} />
+          <Route path="/profile" element={<ProfileSettingsPage />} />
+          <Route path="/consent" element={<ConsentPage />} />
+          <Route path="/myshelf" element={<MyShelfPage />} />
+          <Route path="/product/:id" element={<ProductDetailsPage />} />
+          <Route path="/routine-builder" element={<RoutineBuilderPage />} />
+          <Route path="/history" element={<HistoryPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="**" element={<Navigate to="/" replace />} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
